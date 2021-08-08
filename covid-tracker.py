9ed813414fa4da370f26cd40dc4a94eab5919938
shoemjoh <86920229+shoemjoh@users.noreply.github.com>
2021-08-08 12:28:10 -0400

    Access COVID API Data

---
+++ covid-tracker.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import json
+import statistics
+
+
+## product_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+#productx_url = "https://covid-19.dataflowkit.com/v1/world"
+# write code here
+# 1) View the data in the browser, 2) then make a request for the data, 3) then process the data.
+# beautiful soup would process html looking responses
+#
+#
+#response = requests.get(productx_url)
+# print(type(response))  # <class 'requests.models.Response'>
+# dir(response) - what can we do with the response object
+#
+# print(response.status_code)
+# print(response.text)  # string data type
+#
+#y = json.loads(response.text)
+# print(type(y))
+#
+#print(y["Active Cases_text"])
+
+
+# Calculate and return percent change?
+# Format
+
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("api_k")
+
+url = "https://covid-19-tracking.p.rapidapi.com/v1"
+
+headers = {
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': "covid-19-tracking.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
