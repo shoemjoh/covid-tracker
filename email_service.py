f8837a588dc48297d506ea74dd49c5945095841a
shoemjoh <86920229+shoemjoh@users.noreply.github.com>
2021-08-10 19:48:28 -0400

    Update Production Environment

--- email_service.py
+++ email_service.py
@@ -9,7 +9,7 @@ SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
 
 
-def send_email(subject="[Daily Briefing] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
+def send_email(subject="Daily Covid Update", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
     """
     Sends an email with the specified subject and html contents to the specified recipient,
     If recipient is not specified, sends to the admin's sender address by default.
