4ecd03527e6647ae027fadc1ebee33d067e47d99
shoemjoh <86920229+shoemjoh@users.noreply.github.com>
2021-08-10 20:50:40 -0400

    Updated code to build geography function

--- covid-tracker.py
+++ covid-tracker.py
@@ -6,6 +6,8 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("api_k")
+home_c = os.getenv("home_country")
+
 
 url = "https://covid-19-tracking.p.rapidapi.com/v1"
 
@@ -19,14 +21,21 @@ parsed_response = json.loads(response.text)
 
 
 # print(response.text)
-# print(parsed_response)
+print(parsed_response)
 
-home_country = input("Please enter your home country: ")
+
+# def set_country():
+#    if APP_ENV == "development":
+#        user_country = input("PLEASE INPUT A COUNTRY CODE (e.g. 'US'): ")
+#    else:
+#        user_country = home_c
+#    return user_country
+
 
 for x in parsed_response:
-    if x["Country_text"] == home_country:
+    if x["Country_text"] == home_c:
         print(
-            f"The total case count {home_country} is: ", x["Total Cases_text"])
+            f"The total case count in {home_c} is: ", x["Total Cases_text"])
         break
 
 
