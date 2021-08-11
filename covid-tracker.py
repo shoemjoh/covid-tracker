064f56b4245ff9af4ec03e06dd4a547ac75951f4
shoemjoh <86920229+shoemjoh@users.noreply.github.com>
2021-08-10 22:49:26 -0400

    Updated functions to get cases

--- covid-tracker.py
+++ covid-tracker.py
@@ -5,46 +5,65 @@ import json
 
 from dotenv import load_dotenv
 load_dotenv()
-api_key = os.getenv("api_k")
-home_c = os.getenv("home_country")
 
+API_KEY = os.getenv("api_k")
+HOME_C = os.getenv("home_country")
+APP_ENV = os.getenv("APP_ENV", default="development")
 
-url = "https://covid-19-tracking.p.rapidapi.com/v1"
-
-headers = {
-    'x-rapidapi-key': api_key,
-    'x-rapidapi-host': "covid-19-tracking.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers)
-parsed_response = json.loads(response.text)
-
-
-# print(response.text)
-print(parsed_response)
-
-
-# def set_country():
-#    if APP_ENV == "development":
-#        user_country = input("PLEASE INPUT A COUNTRY CODE (e.g. 'US'): ")
-#    else:
-#        user_country = home_c
-#    return user_country
-
-
-for x in parsed_response:
-    if x["Country_text"] == home_c:
-        print(
-            f"The total case count in {home_c} is: ", x["Total Cases_text"])
-        break
-
-
-# def set_geography():
-#    if APP_ENV == "development":
-#        user_country = input("PLEASE INPUT A COUNTRY CODE (e.g. 'US'): ")
-#        user_zip = input("PLEASE INPUT A ZIP CODE (e.g. 20057): ")
-#    else:
-#        user_country = COUNTRY_CODE
-#        user_zip = ZIP_CODE
-#    return user_country, user_zip
+# # Retrieving from COVID API Tracker:
 #
+# url = "https://covid-19-tracking.p.rapidapi.com/v1"
+# headers = {
+#     'x-rapidapi-key': api_key,
+#     'x-rapidapi-host': "covid-19-tracking.p.rapidapi.com"
+# }
+# response = requests.request("GET", url, headers=headers)
+# parsed_response = json.loads(response.text)
+# print(type(parsed_response))
+
+# Defining which country to pull data for based on user input or default setting.
+
+
+def set_country():
+    if APP_ENV == "development":
+        user_country = input("PLEASE INPUT A COUNTRY CODE (e.g. 'USA'): ")
+    else:
+        user_country = HOME_C
+    return user_country
+
+# Finding the total number of cases, updated daily, for the specified country.
+
+
+country_chosen = set_country()
+
+
+def get_cases(country_id):
+    # country_cases = 100
+    url = "https://covid-19-tracking.p.rapidapi.com/v1"
+    headers = {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': "covid-19-tracking.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers)
+    parsed_response = json.loads(response.text)
+
+    for x in parsed_response:
+        if x["Country_text"] == country_id:
+            country_cases = x["Total Cases_text"]
+            return country_cases
+    return print("No country match found.")
+
+
+if __name__ == "__main__":
+    print(
+        f"RUNNING THE COVID TRACKER SERVICE IN {APP_ENV.upper()} MODE...")
+
+    # Get data from get_cases function.
+    print("Country:", country_chosen)
+
+    result = get_cases(country_id=country_chosen)
+    if not result:
+        print("INVALID GEOGRAPHY. PLEASE CHECK YOUR INPUTS AND TRY AGAIN!")
+        exit()
+
+    print(f"The total COVID-19 case count for {country_chosen} is: ", result)
